41fd0313cb1d0bd3c6842461351723e20246277e
Steven Bore <smtbore@gmail.com>
2020-08-27 14:37:30 +0100

    logic for service 2 & service 4 logic to display service 2 data written

---
+++ service-2/application/routes.py
@@ -0,0 +1,10 @@
+from flask import request,Response
+import requests
+from application import app
+import random
+
+@app.route('/get_strat', methods=['GET'])
+def get_strat():
+  strats = ['Bombsite A', 'Bombsite B', 'Mid Rush', 'Long Rush']
+  strat = strats[random.randrange(4)]
+  return Response(strat, mimetype='text/plain')

4a8eb538c7a1df5d2d7eda3e06b0d2a7c404ea18
Steven Bore <smtbore@gmail.com>
2020-08-27 15:22:51 +0100

    fixed service 2 url in service 4 routes

--- service-4/application/routes.py
+++ service-4/application/routes.py
@@ -12,5 +12,5 @@ def home():
 @app.route('/generate_strat', methods=['GET', 'POST'])
 def generate_strat():
   map = requests.get('http://34.105.142.187:5001/get_map')
-  strat = requests.get('http://34.105.142.187:5002/get_map')
+  strat = requests.get('http://34.105.142.187:5002/get_strat')
   return render_template('strat.html', map_name = map.text, strategy=strat.text, title='Strategy')

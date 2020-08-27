63fee02e3f8596483f6ff799697282f7c0e5d0a6
Steven Bore <smtbore@gmail.com>
2020-08-27 12:25:12 +0100

    QS2-12 issue complete QS2-7 User story complete

--- service-4/application/routes.py
+++ service-4/application/routes.py
@@ -12,4 +12,4 @@ def home():
 @app.route('/generate_strat', methods=['GET', 'POST'])
 def generate_strat():
   map = requests.get('http://34.105.142.187:5001/get_map')
-  return render_template('strat.html', map_name = map.text)
+  return render_template('strat.html', map_name = map.text, title='Strategy')

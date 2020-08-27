75f0eafda8f63265fd6fd4d4c45dfcac02db1b2c
Steven Bore <smtbore@gmail.com>
2020-08-27 22:08:05 +0100

    added to service 3 for the strategy rule to be dependent on the map

--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -6,7 +6,6 @@ import random
 
 @app.route('/get_map', methods=['GET'])
 def get_map():
-  maps = ['Dust 2', 'Cobblestone', 'Inferno', 'Train',
-          'Nuke', 'Mirage', 'Overpass', 'Vertigo', 'Cache']
+  maps = ['Dust 2', 'Cobblestone', 'Inferno', 'Mirage', 'Cache']
   map = maps[random.randrange(9)]
   return Response(map, mimetype='text/plain')

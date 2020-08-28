1dfc6dde0c7b4088240e40defc1c4834bc4ca381
Steven Bore <smtbore@gmail.com>
2020-08-28 14:14:06 +0100

    service 3 tests for all other maps

--- service-3/testing/test_service_3.py
+++ service-3/testing/test_service_3.py
@@ -27,42 +27,42 @@ class TestService3(TestBase):
             data='Dust 2')
         self.assertIn(b'Gladiator: Only one person may leave spawn. When that person dies, the next person leaves', response.data)
 
-  # def test_map_rule2(self):
-  #   with patch('requests.get') as g:
-  #     g.return_value.text = 'Cobblestone'
-  #     with patch('random.randrange') as r:
-  #       r.return_value = 0
-  #       response = self.client.post(
-  #           url_for('get_rule'),
-  #           data='Cobblestone')
-  #       self.assertIn(b'Golden Gun: When the first person on your team gets a kill, everyone else must throw away their weapons and share the weapon that the first kill was made with (the golden gun)', response.data)
+  def test_map_rule2(self):
+    with patch('requests.get') as g:
+      g.return_value.text = 'Cobblestone'
+      with patch('random.randrange') as r:
+        r.return_value = 0
+        response = self.client.post(
+            url_for('get_rule'),
+            data='Cobblestone')
+        self.assertIn(b'Golden Gun: When the first person on your team gets a kill, everyone else must throw away their weapons and share the weapon that the first kill was made with (the golden gun)', response.data)
   
-  # def test_map_rule3(self):
-  #   with patch('requests.get') as g:
-  #     g.return_value.text = 'Inferno'
-  #     with patch('random.randrange') as r:
-  #       r.return_value = 0
-  #       response = self.client.post(
-  #           url_for('get_rule'),
-  #           data='Inferno')
-  #       self.assertIn(b'Juan Deag: All 5 buy deagles - Go juan or go home!', response.data)
+  def test_map_rule3(self):
+    with patch('requests.get') as g:
+      g.return_value.text = 'Inferno'
+      with patch('random.randrange') as r:
+        r.return_value = 0
+        response = self.client.post(
+            url_for('get_rule'),
+            data='Inferno')
+        self.assertIn(b'Juan Deag: All 5 buy deagles - Go juan or go home!', response.data)
 
-  # def test_map_rule4(self):
-  #   with patch('requests.get') as g:
-  #     g.return_value.text = 'Mirage'
-  #     with patch('random.randrange') as r:
-  #       r.return_value = 0
-  #       response = self.client.post(
-  #           url_for('get_rule'),
-  #           data='Mirage')
-  #       self.assertIn(b'Juan Taps Only: You may only fire 1 shot at a time. You must drop or reload your weapon before shooting again', response.data)
+  def test_map_rule4(self):
+    with patch('requests.get') as g:
+      g.return_value.text = 'Mirage'
+      with patch('random.randrange') as r:
+        r.return_value = 0
+        response = self.client.post(
+            url_for('get_rule'),
+            data='Mirage')
+        self.assertIn(b'Juan Taps Only: You may only fire 1 shot at a time. You must drop or reload your weapon before shooting again', response.data)
   
-  # def test_map_rule5(self):
-  #   with patch('requests.get') as g:
-  #     g.return_value.text = 'Cache'
-  #     with patch('random.randrange') as r:
-  #       r.return_value = 0
-  #       response = self.client.post(
-  #           url_for('get_rule'),
-  #           data='Cache')
-  #       self.assertIn(b'Clay Pigeon Shooting: Everyone buys a shotgun and must yell "PULL" whenever you fire at an enemy', response.data)
+  def test_map_rule5(self):
+    with patch('requests.get') as g:
+      g.return_value.text = 'Cache'
+      with patch('random.randrange') as r:
+        r.return_value = 0
+        response = self.client.post(
+            url_for('get_rule'),
+            data='Cache')
+        self.assertIn(b'Clay Pigeon Shooting: Everyone buys a shotgun and must yell "PULL" whenever you fire at an enemy', response.data)

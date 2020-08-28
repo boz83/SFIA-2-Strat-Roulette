58ac00e1a8c20b98d1db6e684485c92b22694ff0
Steven Bore <smtbore@gmail.com>
2020-08-28 11:28:22 +0100

    tests for service 2 written

--- service-1/testing/test_service_1.py
+++ service-1/testing/test_service_1.py
@@ -7,12 +7,19 @@ class TestBase(TestCase):
   def create_app(self):
     config_name="testing"
     return app
+  
+  def setUp(self):
+    pass
+  
+  def tearDown(self):
+    pass
+  
 
 class TestService1(TestBase):
   def test_get_map(self):
     response=self.client.get(url_for('get_map'))
     result=False
-    for item in ['Dust 2', 'Cobblestone', 'Inferno', 'Mirage', 'Cache']:
-      if bytes.decode(response.data) == item:
+    for maps in ['Dust 2', 'Cobblestone', 'Inferno', 'Mirage', 'Cache']:
+      if bytes.decode(response.data) == maps:
         result = True
     self.assertTrue(result)

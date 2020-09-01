a75910ed1334c4294c327a920a41fc0d7c0719f2
Steven Bore <smtbore@gmail.com>
2020-09-01 02:41:41 +0100

    syntax error fix

--- scripts/config.sh
+++ scripts/config.sh
@@ -9,4 +9,12 @@ mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 pip3 install --user ansible
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker $(whoami)
+sudo apt update
+sudo apt install -y curl jq
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose build
 ansible-playbook -i inventory playbook.yaml
